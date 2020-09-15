452612a4ce9bb646492525cd19df4aed82ad0183
Raphael Nestler <raphael.nestler@gmail.com>
2020-09-15 12:56:26 +0200

    Use piratebay API and generate magnet links from them
    While we don't have trackers in the generated magnet links, just using
    the hash works good enough in practice.

--- src/search_providers/pirate_bay_search.rs
+++ src/search_providers/pirate_bay_search.rs
@@ -4,12 +4,26 @@ use crate::torrent::Torrent;
 use async_trait::async_trait;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
-use log::{error, info};
-use select::document::Document;
-use select::node::Node;
-use select::predicate::{Attr, Class, Name};
+use log::info;
+use serde::Deserialize;
 
 use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+pub struct Entry {
+    id: String,
+    name: String,
+    info_hash: String,
+    leechers: String,
+    seeders: String,
+    num_files: String,
+    size: String,
+    username: String,
+    added: String,
+    status: String,
+    category: String,
+    imdb: String,
+}
 
 pub struct PirateBaySearch {
     connection: Client<HttpsConnector<hyper::client::HttpConnector>>,
@@ -27,7 +41,7 @@ impl PirateBaySearch {
 impl SearchProvider for PirateBaySearch {
     async fn search(&self, term: &str) -> Result<Vec<Torrent>, Box<dyn Error + Send + Sync>> {
         info!("Searching on PirateBay");
-        let url = format!("https://thepiratebay.org/search/{}/0/99/0", term);
+        let url = format!("https://apibay.org/q.php?q={}", term);
         let res = self.connection.get(url.parse().unwrap()).await?;
 
         info!("Status: {}", res.status());
@@ -39,9 +53,7 @@ impl SearchProvider for PirateBaySearch {
         }
 
         let body = String::from_utf8(bytes)?;
-
-        let document = Document::from(&*body);
-        Ok(parse_piratebay(&document))
+        parse_piratebay(&body)
     }
 
     fn get_name(&self) -> &'static str {
@@ -49,59 +61,29 @@ impl SearchProvider for PirateBaySearch {
     }
 }
 
-fn parse_piratebay_entry(row: &Node<'_>) -> Result<Torrent, String> {
-    let name = row
-        .find(Class("detLink"))
-        .nth(0)
-        .ok_or_else(|| "Could not find 'detLink'".to_owned())
-        .and_then(|n| Ok(n.text()))?;
+fn parse_piratebay(content: &str) -> Result<Vec<Torrent>, Box<dyn Error + Send + Sync>> {
+    let entries: Vec<Entry> = serde_json::from_str(content)?;
 
-    let link = row
-        .find(Attr("title", "Download this torrent using magnet"))
-        .nth(0)
-        .ok_or_else(|| "Could not find magnet link".to_owned())?;
-    // table data is |Type|Name|Seeders|Leechers|
-    let tds = row.find(Name("td"));
-    let mut tds = tds.skip(2);
-    let seeders = tds.next().and_then(|v| v.text().parse::<u32>().ok());
-    let leechers = tds.next().and_then(|v| v.text().parse::<u32>().ok());
-
-    let magnet_link = link
-        .attr("href")
-        .ok_or_else(|| "Could not find href element".to_owned())?;
-
-    Ok(Torrent {
-        name,
-        magnet_link: magnet_link.to_owned(),
-        seeders,
-        leechers,
-    })
-}
-
-fn parse_piratebay(document: &Document) -> Vec<Torrent> {
-    let search_result = document.find(Attr("id", "searchResult")).into_selection();
-
-    let mut result = vec![];
-    // iterate table rows but skip header
-    for table_row in search_result.find(Name("tr")).iter().skip(1) {
-        match parse_piratebay_entry(&table_row) {
-            Ok(torrent) => result.push(torrent),
-            Err(e) => error!("{}", e),
-        }
-    }
-    result
+    let results = entries
+        .iter()
+        .map(|entry| Torrent {
+            name: entry.name.clone(),
+            magnet_link: format!("magnet:?xt=urn:btih:{}", entry.info_hash),
+            seeders: entry.seeders.parse().ok(),
+            leechers: entry.leechers.parse().ok(),
+        })
+        .collect();
+    Ok(results)
 }
 
 #[cfg(test)]
 mod test {
-    use select::document::Document;
-    static TEST_DATA: &str = include_str!("test_data/piratebay.html");
+    static TEST_DATA: &str = include_str!("test_data/piratebay.json");
 
     #[test]
     fn test_parse_piratebay() {
-        let document = Document::from(TEST_DATA);
-        let torrents = super::parse_piratebay(&document);
-        assert_eq!(torrents.len(), 16);
+        let torrents = super::parse_piratebay(TEST_DATA).unwrap();
+        assert_eq!(torrents.len(), 100);
         for torrent in torrents.iter() {
             assert!(torrent.magnet_link.starts_with("magnet:?"));
             assert!(torrent.seeders.is_some());
